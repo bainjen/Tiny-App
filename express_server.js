94dec8b4b598f97cbfc6f10ee9e21026cddb0523
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-01 02:28:15 +0000

    'dynamically store the url database keys and values according to user input, where the random string is the short url's value and urlDatabase[req.params.shortURL]; is the long url's value.

--- express_server.js
+++ express_server.js
@@ -10,11 +10,11 @@ const getRandomString = (numOfChars) => {
   for (let i = 0; i < numOfChars; i++) {
     randomCharsStr += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
   }
-  return randomCharsStr; 
-}; 
+  return randomCharsStr;
+};
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true})); //convert req body from buffer to string, then add the data to the req(request) object under the key body.
+app.use(bodyParser.urlencoded({ extended: true })); //convert req body from buffer to string, then add the data to the req(request) object under the key body.
 
 const urlDatabase = {
   // shortURL: longURL, 
@@ -24,10 +24,6 @@ const urlDatabase = {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
 });
 
 
@@ -45,13 +41,17 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let tempShortUrl = getRandomString(6); 
+  //we saw long url consoled when enterd req.body so this is how we access that value and set it to the short URL string
+  urlDatabase[tempShortUrl] = req.body.longURL; 
+  res.redirect(`urls/${tempShortUrl}`)
 });
 
+app.get('u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]; 
+  res.redirect(longURL); 
+})
 //post request has a body, a get request does not. this is how we submit POST requests. request body is sent as a buffer. - install body-parser as middleman to make it readable.  
-app.post("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
 
 //The GET /urls/new route needs to be defined before the GET /urls/:id route. Routes defined earlier will take precedence, so if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get("/urls/:id", ...) because Express will think that new is a route parameter. Routes should be ordered from most specific to least specific.
 
@@ -73,33 +73,50 @@ app.get("/urls/:shortURL", (req, res) => {
 // const { url, method } = request; 
 // const render = require('render');
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
 // });
 
-//the templateVars object contains the string 'Hello World' under the key greeting. We then pass the templateVars object to the template called hello_world.
+// app.get("/urls.json", (req, res) => {
+  //   res.json(urlDatabase);
+  // });
+  
+  //the templateVars object contains the string 'Hello World' under the key greeting. We then pass the templateVars object to the template called hello_world.
 
 // app.get("/hello", (req, res) => {
-  //   let templateVars = { greeting: 'Hello World!' };
-  //   res.render("hello_world", templateVars);
-  // });
-  
-  //In our hello_world.ejs file, we can display the 'Hello World!' string stored in the templateVars object by calling the key greeting:
-  // <!-- This would display the string "Hello World!" -->
-  // <h1><%= greeting %></h1>
-  
-  //example of html addition
-  // app.get("/hello", (req, res) => {
-  //   res.send("<html><body>Hello <b>World</b></body></html>\n");
-  // });
+//   let templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+// });
 
-  //example of how they don't connect
-  
-  // app.get("/set", (req, res) => {
-    //   const a = 1;
-    //   res.send(`a = ${a}`);
-    // });
-    
-    // app.get("/fetch", (req, res) => {
-      //   res.send(`a = ${a}`);
-      // });
+//In our hello_world.ejs file, we can display the 'Hello World!' string stored in the templateVars object by calling the key greeting:
+// <!-- This would display the string "Hello World!" -->
+// <h1><%= greeting %></h1>
+
+//example of html addition
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+//example of how they don't connect
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
+
+
+//I think this was a mistake
+// app.post("/urls/new", (req, res) => {
+//   res.render("urls_new");
+// });
+
+//showed us how the app would post ok every time we input text into box and logged it to console.
+// app.post("/urls", (req, res) => {
+//   console.log(req.body);  // Log the POST request body to the console
+//   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+//   res.redirect
+// });
