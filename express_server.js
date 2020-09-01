b643743506634f8bc898bce4adde2afb1d6fcef0
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-09-01 00:17:46 +0000

    implement app.get ur urls/new. This renders the template to present the form to the user. Must go above GET/urls/:id to take precedence.

--- express_server.js
+++ express_server.js
@@ -10,18 +10,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 //sharing data with urls_index.ejs
 app.get('/urls', (req, res) => {
@@ -29,10 +28,23 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+//GET route to render the urls_new.ejs template (given below) in the browser, to present the form to the user; and a POST route to handle the form submission.
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//post request has a body, a get request does not.
+app.post("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//The GET /urls/new route needs to be defined before the GET /urls/:id route. Routes defined earlier will take precedence, so if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get("/urls/:id", ...) because Express will think that new is a route parameter. Routes should be ordered from most specific to least specific.
 
 app.get("/urls/:shortURL", (req, res) => {
   //shortURL --> I am assigning a value from the request params, which I have called shortURL --> longURL -->I am accessing a value; 
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
   // console.log("request object", req);
   // console.log("response object", res);
   console.log(req.params);
@@ -40,9 +52,6 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 
 
@@ -57,19 +66,23 @@ app.listen(PORT, () => {
   //   let templateVars = { greeting: 'Hello World!' };
   //   res.render("hello_world", templateVars);
   // });
-
+  
   //In our hello_world.ejs file, we can display the 'Hello World!' string stored in the templateVars object by calling the key greeting:
   // <!-- This would display the string "Hello World!" -->
   // <h1><%= greeting %></h1>
-
+  
+  //example of html addition
+  // app.get("/hello", (req, res) => {
+  //   res.send("<html><body>Hello <b>World</b></body></html>\n");
+  // });
 
   //example of how they don't connect
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
+  
+  // app.get("/set", (req, res) => {
+    //   const a = 1;
+    //   res.send(`a = ${a}`);
+    // });
+    
+    // app.get("/fetch", (req, res) => {
+      //   res.send(`a = ${a}`);
+      // });
